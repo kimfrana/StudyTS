0bb1b53f2a43c0b6e4d295f41137cfa2390071c7
kimfrana <kimfkunz@gmail.com>
2021-11-02 15:08:05 -0300

    Functions and Primitives

---
+++ primitives/function/index.ts
@@ -0,0 +1,29 @@
+
+//In functions
+const TakeString = ():string => { //This arrow function return a string
+    const dog: string = "AuAu!"
+    return dog
+}
+
+function getFavoriteNumber(): number {
+    return 666
+}
+    
+const takeName = (name:string) => {
+    return name
+}
+
+const ReturnTwo = () => {
+    const blackCat: string = "gato preto"
+    const blueCat: string = "gato azul"
+    return blackCat // This output is a error bc of the 
+}
+
+//Object
+function printName(obj: {first: string; last?: string}) {
+    //The first argument is required
+    //But the second is optional bc of use of the operator "?"
+}
+
+
+
---
+++ primitives/index.ts
@@ -0,0 +1,34 @@
+// PRIMITIVES
+
+// Daclaring Variables
+
+ //Type String
+let home: string = "I love my home";
+let cat?: string = "This cat is optional" //Optional value
+
+//Type Number
+let numberOne:number = 1
+
+//Type Boolean
+let switchButton: boolean = true; 
+
+//Type Any
+// Can be any type
+let number: any = "I'm a number";
+
+//Type Array
+let arra: number[] = [1,3,2]
+let arrayTwo: string[]= ["one", "two", "three"]
+
+
+//Objects
+
+
+
+
+
+
+
+
+
+
